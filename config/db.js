b3a9a95ec6e40f54c71806e2cda4fbc8720500c5
sandjonyves <sandjonyves@gmail.com>
2025-05-31 00:45:42 +0200

    create docker-compose.uml for connect postgres in projet

--- config/db.js
+++ config/db.js
@@ -1,10 +1,28 @@
 const { Sequelize } = require('sequelize');
-const sqlite3 = require('sqlite3');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'sequelize.sqlite',
-  dialectModule: sqlite3,
-});
+let sequelize;
+
+if (process.env.DB_TYPE === 'postgres') {
+
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      logging: false, 
+    }
+  );
+} else {
+
+  const sqlite3 = require('sqlite3');
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'sequelize.sqlite',
+    dialectModule: sqlite3,
+    logging: false,
+  });
+}
 
 module.exports = sequelize;
